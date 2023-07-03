0e926cd3e641c0066448087287f2472b00d68bb7
Sarathakkireddy <sarathakkireddy@gmail.com>
2023-07-03 13:06:41 +0530

    assignment2 commit

---
+++ assignment2/readFromLine.js
@@ -0,0 +1,9 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Please enter your name:",(answer)=>{
+    console.log("Hello",answer);
+    rl.close();
+})
